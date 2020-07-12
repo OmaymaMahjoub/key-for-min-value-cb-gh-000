dc0f9d14bde15f799751ba73bc069bc413ca7107
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-12 10:19:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,26 +7,21 @@ def key_for_min_value(name_hash)
   else
       arr1=Array.new
       arr2=Array.new
-      puts "done"
       name_hash.collect do |k,v|
         arr1.push(k)
         arr2.push(v)
       end
-      puts "2"
       vm=arr2[0]
       i=0
       pos=0
-      puts "3"
       arr2.each do |v|
         if vm>v
           vm=v
           pos=i
         end
-        puts "4"
         i+=1
       end
-      puts arr1[pos]
+      return arr1[pos]
     end
 end
-ikea = {:chair => 25, :table => 85, :mattress => 450}
-key_for_min_value(ikea)
+
