4018543a75e5773a7469ff53e56e1182a1069bf4
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-12 10:11:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,20 +5,20 @@ def key_for_min_value(name_hash)
   if name_hash.size==0
     return nil
   else
-      name_hash.collect do |key,value|
-        key
-        value
+      name_hash.collect do |ke,val|
+        ke
+        val
       end
-      vm=value[0]
+      vm=val[0]
       i=0
       p=0
-      value.each do |v|
+      val.each do |v|
         if vm>v
           vm=v
           p=i
         end
         i+=1
       end
-      return key[p]
+      return ke[p]
     end
 end
