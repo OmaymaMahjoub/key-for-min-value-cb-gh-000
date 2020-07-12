fd20f4725f56b31982c0ca5c3bfadaab2872a168
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-12 10:18:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,13 +7,16 @@ def key_for_min_value(name_hash)
   else
       arr1=Array.new
       arr2=Array.new
+      puts "done"
       name_hash.collect do |k,v|
         arr1.push(k)
         arr2.push(v)
       end
+      puts "2"
       vm=arr2[0]
       i=0
       pos=0
+      puts "3"
       arr2.each do |v|
         if vm>v
           vm=v
