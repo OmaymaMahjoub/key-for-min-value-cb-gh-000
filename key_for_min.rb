4a6e1ea14d068e61cae6ab7dec173cd31a959ea8
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-12 09:57:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.size==0
+    return nil
+  else
+    k=name_hash.key[0]
+    v=name_hash.value[0]
+    name_hash.collect do |key,value|
+      if value<v
+        k=key
+    end
+  end
+  return k
 end
